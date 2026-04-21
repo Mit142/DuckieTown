d2dfb142f3456badb662655c413a152c345ba0a6
Mit142 <mitalidesh7@gmail.com>
2026-04-21 15:29:57 -0400

    Change script to launch move.py instead of square.py

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 $DT_REPO_PATH/packages/square.py
+dt-exec python3 $DT_REPO_PATH/packages/move.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
