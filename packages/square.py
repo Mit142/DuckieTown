f773fbd8f0d657244fbcd73e3689a861110a8d7e
Mit142 <mitalidesh7@gmail.com>
2026-04-21 13:18:04 -0400

    Adjust left turn speed in square.py

--- packages/square.py
+++ packages/square.py
@@ -41,7 +41,7 @@ class SquareDriverNode(DTROS):
             # 2. Point Turn 90 degrees LEFT (Stronger & Sharper)
             # Both wheels help the turn, reducing motor strain.
             rospy.loginfo(f"Side {i+1}: Point turning left...")
-            self.send_cmd(vel_left=-0.7, vel_right=0.7, duration=0.5) 
+            self.send_cmd(vel_left=-0.35, vel_right=0.35, duration=0.5) 
             self.stop()
         
     def on_shutdown(self):
