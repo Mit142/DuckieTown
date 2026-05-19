2c596876fef8bb37bef2632a1c9aecfee9684b21
Mit142 <mitalidesh7@gmail.com>
2026-05-19 13:41:42 -0400

    Source ROS Noetic setup script in default.sh

--- launchers/default.sh
+++ launchers/default.sh
@@ -7,7 +7,7 @@ dt-launchfile-init
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-
+source /opt/ros/noetic/setup.bash
 
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
