59a435b86bc2488d07dc70444b12287088232016
Mit142 <mitalidesh7@gmail.com>
2026-05-19 13:27:27 -0400

    Change script to launch laneFollowing.py instead of move.py

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 $DT_REPO_PATH/packages/move.py
+dt-exec python3 $DT_REPO_PATH/packages/laneFollowing.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
