8eb0c09b565e2c5c5a18f6d6f3e34011cb54c458
Mit142 <mitalidesh7@gmail.com>
2026-05-21 13:49:36 -0400

    Add wheels_cmd to default.sh for vehicle commands

--- launchers/default.sh
+++ launchers/default.sh
@@ -19,7 +19,8 @@ dt-exec python3 $DT_REPO_PATH/packages/laneFollowing.py \
     __ns:=/${VEHICLE_NAME} \
     ~image/compressed:=/${VEHICLE_NAME}/camera_node/image/compressed \
     ~lane_pose:=/${VEHICLE_NAME}/lane_filter_node/lane_pose \
-    ~car_cmd:=/${VEHICLE_NAME}/lane_following_node/car_cmd
+    ~car_cmd:=/${VEHICLE_NAME}/lane_following_node/car_cmd \
+    ~car_cmd:=/${VEHICLE_NAME}/wheels_driver_node/wheels_cmd 
  
 
 # ----------------------------------------------------------------------------
