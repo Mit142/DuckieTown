053d05c1e1304ff4fc66c901f3eac3a630acd4f0
Mit142 <mitalidesh7@gmail.com>
2026-05-28 13:22:47 -0400

    Add vehicle identifier to default.sh

--- launchers/default.sh
+++ launchers/default.sh
@@ -20,7 +20,9 @@ dt-exec python3 $DT_REPO_PATH/packages/laneFollowing.py \
     ~image/compressed:=/${VEHICLE_NAME}/camera_node/image/compressed \
     ~lane_pose:=/${VEHICLE_NAME}/lane_filter_node/lane_pose \
     ~car_cmd:=/${VEHICLE_NAME}/lane_following_node/car_cmd \
-    ~car_cmd:=/${VEHICLE_NAME}/wheels_driver_node/wheels_cmd 
+    ~car_cmd:=/${VEHICLE_NAME}/wheels_driver_node/wheels_cmd \
+    ~veh:=entebot208
+
  
 
 # ----------------------------------------------------------------------------
